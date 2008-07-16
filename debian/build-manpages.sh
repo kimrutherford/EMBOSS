c89a5a4d9e4288485fdbe7defb0b92293bb1e81c
Charles Plessy <plessy@debian.org>
2008-07-16 06:48:32 +0000

    Added build-manpages.sh to build the manpages, and updated acd2docbook.pl.

---
+++ debian/build-manpages.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Copyleft 2007 Charles Plessy, do what you want with this trivial file.
+
+set -v
+set -e
+
+PACKAGE=$(dpkg-parsechangelog -ldebian/changelog | grep Source: | cut -f2 -d' ')
+VERSION=$(dpkg-parsechangelog -ldebian/changelog | grep Version: | cut -f2 -d' ' | cut -f1 -d-)
+ORIGTARGZ=../${PACKAGE}_${VERSION}.orig.tar.gz
+
+rm -f debian/$PACKAGE.manpages
+
+for FILE in $(tar --list --gzip --file $ORIGTARGZ | grep \\.acd | grep -v /test/)
+  do
+  ACD=$(basename $FILE .acd)
+  tar --verbose --gzip --file $ORIGTARGZ --extract $FILE --to-stdout \
+  | ../../emboss/trunk/debian/acd2docbook.pl /dev/stdin ${VERSION} \
+  | xsltproc --nonet --param man.charmap.use.subset 0 -o debian/manpages/ -
+  echo "debian/manpages/$ACD.1e" >> debian/$PACKAGE.manpages
+done
