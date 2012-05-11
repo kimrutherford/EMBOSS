1526a27486258ced21019da7823b8b07c877bb41
Charles Plessy <plessy@debian.org>
2012-05-11 14:17:22 +0900

    Obtain the ACD files from the Git repository.

--- debian/build-manpages.sh
+++ debian/build-manpages.sh
@@ -1,8 +1,5 @@
-#!/bin/sh
+#!/bin/sh -e
 # Copyleft 2007 Charles Plessy, do what you want with this trivial file.
-
-set -v
-set -e
 
 PACKAGE=$(dpkg-parsechangelog -ldebian/changelog | grep Source: | cut -f2 -d' ')
 VERSION=$(dpkg-parsechangelog -ldebian/changelog | grep Version: | cut -f2 -d' ' | cut -f1 -d-)
@@ -10,11 +7,10 @@ ORIGTARGZ=../${PACKAGE}_${VERSION}.orig.tar.gz
 
 rm -f debian/$PACKAGE.manpages
 
-for FILE in $(tar --list --gzip --file $ORIGTARGZ | grep \\.acd | grep -v /test/)
+for FILE in $(ls emboss/acd/*.acd)
   do
   ACD=$(basename $FILE .acd)
-  tar --verbose --gzip --file $ORIGTARGZ --extract $FILE --to-stdout \
-  | ./debian/acd2docbook.pl /dev/stdin ${VERSION} \
+  ./debian/acd2docbook.pl $FILE ${VERSION} \
   | xsltproc --nonet --param man.charmap.use.subset 0 -o debian/manpages/ -
   echo "debian/manpages/$ACD.1e" >> debian/$PACKAGE.manpages
 done
